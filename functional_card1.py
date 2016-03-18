17008083ab7bc23487867d8f786f40259adf1689
Diego Toledo <dtoledo23@users.noreply.github.com>
2016-03-17 19:44:47 -0600

    Message display changed

--- functional_card1.py
+++ functional_card1.py
@@ -90,8 +90,8 @@ while True:
     myLcd.setColor(red, green, 0)
 
     messages = "Disponibles: " + str(lugares) + " "
-    myLcd.setCursor(0,5)
-    myLcd.write("Zona A")
+    myLcd.setCursor(0,1)
+    myLcd.write("P_Residencias")
     myLcd.setCursor(1,0)
     myLcd.write(messages)
 
